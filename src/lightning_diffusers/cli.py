21ea10c67d2d42357a2465dfc054ae31be1b3d74
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-04-23 16:42:59 +0900

    update for optimizer

--- src/lightning_diffusers/cli.py
+++ src/lightning_diffusers/cli.py
@@ -1,15 +1,9 @@
 from lightning.pytorch.cli import LightningCLI
 
-from lightning_diffusers.data import MnistDataModule
-from lightning_diffusers.models import MnistDDPMModule
-
 
 def main() -> None:
     LightningCLI(
-        model_class=MnistDDPMModule,
-        datamodule_class=MnistDataModule,
         subclass_mode_model=True,
         subclass_mode_data=True,
-        auto_configure_optimizers=True,
         save_config_kwargs={"overwrite": True},
     )
